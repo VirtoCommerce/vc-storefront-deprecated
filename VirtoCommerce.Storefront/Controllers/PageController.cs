a31086f3fb1e09f8046484f4f6a2c7207f9d9667
artem-dudarev <ad@virtoway.com>
2016-07-22 13:01:50 +0200

    Fixed page title for CMS pages

--- VirtoCommerce.Storefront/Controllers/PageController.cs
+++ VirtoCommerce.Storefront/Controllers/PageController.cs
@@ -21,32 +21,45 @@ namespace VirtoCommerce.Storefront.Controllers
         public ActionResult GetContentPage(ContentItem page)
         {
             var blogArticle = page as BlogArticle;
-            var contentPage = page as ContentPage;
             if (blogArticle != null)
             {
                 WorkContext.CurrentBlogArticle = blogArticle;
                 WorkContext.CurrentBlog = WorkContext.Blogs.SingleOrDefault(x => x.Name.Equals(blogArticle.BlogName, StringComparison.OrdinalIgnoreCase));
                 return View("article", page.Layout, WorkContext);
             }
-            else
-            {
-                WorkContext.CurrentPage = contentPage;
-                return View("page", page.Layout, WorkContext);
-            }
+
+            var contentPage = page as ContentPage;
+            SetCurrentPage(contentPage);
+            return View("page", page.Layout, WorkContext);
         }
 
         // GET: /pages/{page}
         public ActionResult GetContentPageByName(string page)
         {
-            var contentPages = WorkContext.Pages.Where(x => string.Equals(x.Url, page, StringComparison.OrdinalIgnoreCase));
-            var contentPage = contentPages.FindWithLanguage(WorkContext.CurrentLanguage);
+            var contentPage = WorkContext.Pages
+                .OfType<ContentPage>()
+                .Where(x => string.Equals(x.Url, page, StringComparison.OrdinalIgnoreCase))
+                .FindWithLanguage(WorkContext.CurrentLanguage);
+
             if (contentPage != null)
             {
-                WorkContext.CurrentPage = contentPage as ContentPage;
-
+                SetCurrentPage(contentPage);
                 return View("page", WorkContext);
             }
-            throw new HttpException(404, "Page with " + page + " not found.");
+
+            throw new HttpException(404, "Page not found. Page URL: '" + page + "'.");
+        }
+
+
+        private void SetCurrentPage(ContentPage contentPage)
+        {
+            WorkContext.CurrentPage = contentPage;
+            WorkContext.CurrentPageSeo = new SeoInfo
+            {
+                Language = contentPage.Language,
+                Title = contentPage.Title,
+                Slug = contentPage.Permalink
+            };
         }
     }
 }
