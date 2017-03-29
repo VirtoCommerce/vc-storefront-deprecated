4034561681d251ba9018e0599c78b5b7b4033356
artem-dudarev <ad@virtoway.com>
2017-03-29 12:40:52 +0200

    Disabled retries for API clients

---
+++ VirtoCommerce.Storefront/Common/ServiceClientExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Rest;
+
+namespace VirtoCommerce.Storefront.Common
+{
+    public static class ServiceClientExtensions
+    {
+        public static T DisableRetries<T>(this T client)
+            where T : ServiceClient<T>
+        {
+            client.SetRetryPolicy(null);
+            return client;
+        }
+    }
+}
