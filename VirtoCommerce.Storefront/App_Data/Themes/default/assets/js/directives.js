d2d17ac427c63e70ae378fb589489ea7498803bd
ksa <ksa@virtoway.com>
2017-06-28 17:41:26 +0200

    Fallback directive for non existing image

--- VirtoCommerce.Storefront/App_Data/Themes/default/assets/js/directives.js
+++ VirtoCommerce.Storefront/App_Data/Themes/default/assets/js/directives.js
@@ -11,3 +11,17 @@ storefrontApp.directive('vcContentPlace', ['marketingService', function (marketi
         replace: true
     }
 }]);
+
+storefrontApp.directive('vcFallbackImage', function () {
+    return {
+        link: function (scope, element, attrs) {
+            element.on('error', function () {
+                var start = attrs.src.lastIndexOf('_');
+                var end = attrs.src.lastIndexOf('.');
+                var resolution = attrs.src.slice(start, end);
+                var fallbackImage = attrs.src.replace(resolution, '');
+                element.attr('src', fallbackImage);
+            });
+        }
+    };
+});
