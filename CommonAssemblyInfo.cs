142b350bf5e46336df506886ce86ef8a98fcd465
Eugeny Tatarincev <et@virtoway.com>
2017-07-11 11:27:13 +0200

    [publish] 2.21.5

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -1,4 +1,4 @@
-// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+﻿// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 // <copyright file="GlobalAssemblyInfo.cs" company="VirtoCommerce">
 //   Copyright © VirtoCommerce. All rights reserved.
 // </copyright>
@@ -15,8 +15,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Virto Commerce Storefront")]
 [assembly: AssemblyCopyright("Copyright © VirtoCommerce 2011-2016")]
 
-[assembly: AssemblyFileVersion("2.21.4.0")]
-[assembly: AssemblyVersion("2.21.4.0")]
+[assembly: AssemblyFileVersion("2.21.5.0")]
+[assembly: AssemblyVersion("2.21.5.0")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
