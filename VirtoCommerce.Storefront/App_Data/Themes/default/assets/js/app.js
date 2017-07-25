45320f8ad79f27117ec2e4aaa6b31ce81f8bcc02
Eugeny Tatarincev <et@virtoway.com>
2017-07-25 08:05:58 +0200

    Fix script errors on Add to list popup

--- VirtoCommerce.Storefront/App_Data/Themes/default/assets/js/app.js
+++ VirtoCommerce.Storefront/App_Data/Themes/default/assets/js/app.js
@@ -10,19 +10,23 @@ storefrontApp.factory('httpErrorInterceptor', ['$q', '$rootScope', function ($q,
     var httpErrorInterceptor = {};
 
     httpErrorInterceptor.responseError = function (rejection) {
-        $rootScope.$broadcast('storefrontError', {
-            type: 'error',
-            title: [rejection.config.method, rejection.config.url, rejection.status, rejection.statusText, rejection.data.message].join(' '),
-            message: rejection.data.stackTrace,
-        });
+        if (rejection.data && rejection.data.message) {
+            $rootScope.$broadcast('storefrontError', {
+                type: 'error',
+                title: [rejection.config.method, rejection.config.url, rejection.status, rejection.statusText, rejection.data.message].join(' '),
+                message: rejection.data.stackTrace,
+            });
+        }
         return $q.reject(rejection);
     };
     httpErrorInterceptor.requestError = function (rejection) {
-        $rootScope.$broadcast('storefrontError', {
-            type: 'error',
-            title: [rejection.config.method, rejection.config.url, rejection.status, rejection.statusText, rejection.data.message].join(' '),
-            message: rejection.data.stackTrace,
-        });
+        if (rejection.data && rejection.data.message) {
+            $rootScope.$broadcast('storefrontError', {
+                type: 'error',
+                title: [rejection.config.method, rejection.config.url, rejection.status, rejection.statusText, rejection.data.message].join(' '),
+                message: rejection.data.stackTrace,
+            });
+        }
         return $q.reject(rejection);
     };
 
