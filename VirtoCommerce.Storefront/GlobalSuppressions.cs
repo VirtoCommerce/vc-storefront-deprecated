8311d2d56c0579a8b98bf65cb21b7aad44e213b5
Aleksandr Vishnyakov <av@virtoway.com>
2017-10-03 13:42:00 +0500

    Try to suppress Sonar Qube false positive; Added support of SonarLint

---
+++ VirtoCommerce.Storefront/GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+﻿
+// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Sonar Vulnerability", "S2068:Credentials should not be hard-coded", Justification = "<Pending>", Scope = "member", Target = "~F:VirtoCommerce.Storefront.Common.StorefrontConstants.PasswordResetTokenCookie")]
+
