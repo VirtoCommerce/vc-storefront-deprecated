c4ca47692da9c738c5919d6c29b32fd0d424d230
Valeriy Krashenko <vk@virtoway.com>
2017-11-23 19:11:15 +0200

    add login data to customer account

---
+++ VirtoCommerce.Storefront.Model/Logins.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using VirtoCommerce.Storefront.Model.Common;
+
+namespace VirtoCommerce.Storefront.Model
+{
+    public partial class Logins : ValueObject<Logins>
+    {
+        public string LoginProvider { get; set; }
+        public string ProviderKey { get; set; }
+        public override string ToString()
+        {
+            var retVal = string.Join(" ", ProviderKey, LoginProvider);
+            return retVal;
+        }
+    }
+}
